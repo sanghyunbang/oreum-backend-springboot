80adff92fd78002d42b63fb96e2716c88f9ba40a
IH <inhong0654b@naver.com>
2025-06-05 14:26:24 +0900

    Home / Latout/ AppRoutes / Board 전반
    Approutes 링크추가, 홈 편집, Board 컴포넌트 몇몇 추가.
    글쓰기 아직 구성안됨

--- src/main/java/com/oreum/oreum_backend_springboot/board/boardDTO.java
+++ src/main/java/com/oreum/oreum_backend_springboot/board/boardDTO.java
@@ -1,5 +1,20 @@
 package com.oreum.oreum_backend_springboot.board;
 
+import lombok.Data;
+
+@Data
 public class boardDTO {
-    
+	
+	private int postId;
+    private int userId;
+    private int boardId;
+    private String type; // ENUM: "general", "curation"
+    private String title;
+    private String content;
+    private String imageUrls;
+    private int likeCount;
+    private int commentCount;
+    private boolean isDeleted;
+    private java.sql.Timestamp createdAt;
+    private java.sql.Timestamp updatedAt;
 }
