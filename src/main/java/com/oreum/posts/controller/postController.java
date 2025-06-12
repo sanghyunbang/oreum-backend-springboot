82790b973749da59973058334ee6b27d16e40a9b
HYUN <149238724+sanghyunbang@users.noreply.github.com>
2025-06-13 00:24:34 +0900

    feat: postController API 업로드

--- src/main/java/com/oreum/posts/controller/postController.java
+++ src/main/java/com/oreum/posts/controller/postController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,7 +21,7 @@ import com.oreum.posts.dto.PostsDTO;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/posts")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true") //  CORS 허용
 public class postController {
@@ -28,33 +29,29 @@ public class postController {
 
     private final S3Service s3Service;
 
-    @PostMapping(consumes = "multipart/form-data")
-    public ResponseEntity<List<String>> uploadPost(
-        @RequestParam("title") String title,
-        @RequestParam("content") String content,
-        @RequestParam("category") String category, 
-        @RequestParam("mediaFiles") List<MultipartFile> mediaFiles
-    ) {
-        List<String> uploadedUrls = new ArrayList<>();
+    @PostMapping(value = "/insert", consumes = "multipart/form-data")
+    public ResponseEntity<?> insertPost(@RequestPart("post") PostsDTO postDTO,
+                                        @RequestPart(value = "media", required = false)
+                                        List<MultipartFile> mediaFiles) {
 
-        for (MultipartFile file : mediaFiles) {
-            String uploadedUrl = s3Service.uploadFile(file);
-            uploadedUrls.add(uploadedUrl);
+        // DB 저장하기 1 (게시글 공통 테이블)
+        pd.insertpost(postDTO);
+
+        // DB 저장하기 2 (큐레이션 여부 따져서 큐레이션 저장)
+        if("curation".equals(postDTO.getType())){
+            pd.insertCurationDetail(postDTO);
         }
+        // S3에 업로드 하고 URL 받아서 저장
+        if (mediaFiles != null && !mediaFiles.isEmpty()){
+            for (MultipartFile file : mediaFiles) {
+                String url = s3Service.uploadFile(file);
+                
+                String mediaType = file.getContentType().startsWith("image") ? "image" : "video";
 
-        // TODO: title, content, category, uploadedUrls 를 DB에 저장하는 로직도 나중에 추가 가능
-
-        return ResponseEntity.ok(uploadedUrls);
-    }
-    
-    //게시글 등록
-    @PostMapping("/insert")
-    public ResponseEntity<?> insertposts(@RequestBody PostsDTO postDto) {
-    	
-    	System.out.println("       ☆☆☆☆☆☆☆☆☆☆ 포스트 진입 성공 ☆☆☆☆☆☆☆☆☆☆       ");
-
-    	pd.insertpost(postDto);
-        
+                pd.insertPostMedia(postDTO.getPostId(), mediaType, url);
+            }
+        }        
         return ResponseEntity.ok("게시글 등록 완료");
     }
+ 
 }
