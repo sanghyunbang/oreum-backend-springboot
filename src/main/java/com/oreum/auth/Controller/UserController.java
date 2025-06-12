1fb7a3b83bc2c80cb76dfa683ac8440eb07960e4
HYUN <149238724+sanghyunbang@users.noreply.github.com>
2025-06-13 00:23:05 +0900

    fix: user API 변경 및 포스트 관련 API 추가

--- src/main/java/com/oreum/auth/Controller/UserController.java
+++ src/main/java/com/oreum/auth/Controller/UserController.java
@@ -3,17 +3,22 @@ package com.oreum.auth.Controller;
 import com.oreum.auth.dto.CustomOAuth2User;
 import com.oreum.auth.mapper.UserDao;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true") //  CORS 허용
 public class UserController {
-	@Autowired UserDao ud;
+	@Autowired UserDao userMapper;
 
     @GetMapping("/me")
     public Map<String, Object> getCurrentUser(Authentication authentication) {
@@ -30,13 +35,27 @@ public class UserController {
         );
     }
     
-    @GetMapping("/idget")
-    public ResponseEntity<Integer> getid(@RequestParam String email) {
-    	Integer userId = ud.selectUserIdByEmail(email);
-    	System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡemail로 id조회 진입ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
-    	if (userId == null) {
+    @GetMapping("/nickname")
+    public ResponseEntity<String> getNickname(@RequestParam int userId) {
+    	System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡuserId로 진입ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+
+        String nickName = userMapper.userNameByuserId(userId);
+    	if (nickName == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(userId);
+        return ResponseEntity.ok(nickName);
     }
+
+    @GetMapping("/infoForPost")
+    public ResponseEntity<?> getUserInfo(HttpServletRequest request) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.getPrincipal() instanceof CustomOAuth2User user) {
+            String email = user.getUserName();
+            int userId = userMapper.selectUserIdByEmail(email);
+            String nickname = userMapper.userNameByuserId(userId);
+            return ResponseEntity.ok(Map.of("userId", userId, "nickname", nickname));
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증 실패");
+    }
+
 }
