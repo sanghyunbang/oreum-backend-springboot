2f209c7ea9c3777aeacdf9e5196e3f3956a53ca3
HYUN <149238724+sanghyunbang@users.noreply.github.com>
2025-06-26 16:34:42 +0900

    feat: MongoDB에서 Get하기 위한 Repository

--- src/main/java/com/oreum/posts/dao/CurationSegmentRepository.java
+++ src/main/java/com/oreum/posts/dao/CurationSegmentRepository.java
@@ -3,7 +3,9 @@ package com.oreum.posts.dao;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.oreum.posts.dto.CurationSegmentDoc;
+import java.util.List;
+
 
 public interface CurationSegmentRepository extends MongoRepository<CurationSegmentDoc, String>{
-    
+    List<CurationSegmentDoc> findByPostId(Integer postId);
 }
